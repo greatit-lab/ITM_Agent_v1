a4f9f8402989a0e593555bb69fe049a5b6bd5238
greatit-lab <gil2zum@gmail.com>
2025-11-10 12:04:53 +0900

    Create RegexConfigForm.cs

---
+++ ITM_Agent/ucPanel/RegexConfigForm.cs
@@ -0,0 +1,86 @@
+// ITM_Agent/ucPanel/RegexConfigForm.cs
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ITM_Agent.Properties;
+
+namespace ITM_Agent.ucPanel
+{
+    /// <summary>
+    /// 정규표현식 패턴과 대상 폴더를 설정하기 위한 폼
+    /// </summary>
+    public partial class RegexConfigForm : Form
+    {
+        private readonly string baseFolderPath;
+
+        public string RegexPattern
+        {
+            get => tb_RegInput.Text;
+            set => tb_RegInput.Text = value;
+        }
+
+        public string TargetFolder
+        {
+            get => tb_RegFolder.Text;
+            set => tb_RegFolder.Text = value;
+        }
+
+        public RegexConfigForm(string baseFolderPath)
+        {
+            this.baseFolderPath = baseFolderPath ?? AppDomain.CurrentDomain.BaseDirectory;
+
+            InitializeComponent();
+
+            this.Text = "Regex Configuration";
+            tb_RegFolder.ReadOnly = true;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            btn_RegSelectFolder.Click += btn_RegSelectFolder_Click;
+            btn_RegApply.Click += btn_RegApply_Click;
+            btn_RegCancel.Click += (sender, e) => this.DialogResult = DialogResult.Cancel;
+
+            this.Load += RegexConfigForm_Load;
+        }
+
+        private void RegexConfigForm_Load(object sender, EventArgs e)
+        {
+            // 폼 위치나 초기화 로직 필요 시 추가
+        }
+
+        private void btn_RegSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.SelectedPath = baseFolderPath;
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    TargetFolder = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btn_RegApply_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(RegexPattern))
+            {
+                /* [삭제] MessageBox.Show("정규표현식을 입력해주세요.", "경고", ...); */
+                MessageBox.Show(Resources.MSG_REGEX_REQUIRED,             // [추가]
+                                Resources.CAPTION_WARNING,
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TargetFolder))
+            {
+                /* [삭제] MessageBox.Show("복사 폴더를 선택해주세요.", "경고", ...); */
+                MessageBox.Show(Resources.MSG_FOLDER_REQUIRED,            // [추가]
+                                Resources.CAPTION_WARNING,
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
